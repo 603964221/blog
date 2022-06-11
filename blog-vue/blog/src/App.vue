9e992dd739cbdda862f1c6e0ff5628a25154ae8c
mayun_xy <603934221@qq.com>
2022-06-11 10:31:16 +0800

    本地化

--- blog-vue/blog/src/App.vue
+++ blog-vue/blog/src/App.vue
@@ -22,8 +22,6 @@
     <ForgetModel></ForgetModel>
     <!-- 绑定邮箱模态框 -->
     <EmailModel></EmailModel>
-    <!-- 音乐播放器 -->
-    <Player v-if="blogInfo.websiteConfig.isMusicPlayer == 1 && !isMobile" />
     <!-- 聊天室 -->
     <ChatRoom v-if="blogInfo.websiteConfig.isChatRoom == 1"></ChatRoom>
   </v-app>
@@ -39,7 +37,6 @@ import LoginModel from "./components/model/LoginModel";
 import RegisterModel from "./components/model/RegisterModel";
 import ForgetModel from "./components/model/ForgetModel";
 import EmailModel from "./components/model/EmailModel";
-import Player from "./components/zw-player/player.vue";
 import ChatRoom from "./components/ChatRoom";
 export default {
   created() {
@@ -50,7 +47,6 @@ export default {
   },
   components: {
     TopNavBar,
-    Player,
     SideNavBar,
     Footer,
     BackTop,
