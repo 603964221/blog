b97e584dc6265035413855a8badbc10a206272a9
mayun_xy <603934221@qq.com>
2023-06-09 12:01:49 +0800

    本地化

--- blog-vue/blog/src/assets/js/config.js
+++ blog-vue/blog/src/assets/js/config.js
@@ -1,5 +1,5 @@
 export default {
-  QQ_APP_ID: "101878726",
+  QQ_APP_ID: "825399663",
   QQ_REDIRECT_URI: "https://www.talkxj.com/oauth/login/qq",
   WEIBO_APP_ID: "4039197195",
   WEIBO_REDIRECT_URI: "https://www.talkxj.com/oauth/login/weibo",
--- blog-vue/blog/vue.config.js
+++ blog-vue/blog/vue.config.js
@@ -3,7 +3,8 @@ module.exports = {
   devServer: {
     proxy: {
       "/api": {
-        target: "http://localhost:8082",
+        // target: "http://192.168.100.10:8082",
+        target: "http://127.0.0.1:8082",
         changeOrigin: true,
         pathRewrite: {
           "^/api": ""
@@ -12,6 +13,9 @@ module.exports = {
     },
     port:8081,
     disableHostCheck: true
+  },
+  configureWebpack: {//调试代码用，正式发布时应删除。
+    devtool: 'source-map'
   },
   productionSourceMap: false,
   css: {
